83843e2a23dec4fc3dc215c372446f1b8f1940f3
Paulo Diniz <pdiniz_100@yahoo.com>
2017-01-11 12:32:00 -0800

    add initial tests

---
+++ src/components/course/CourseForm.Enzyme.test.js
@@ -0,0 +1,31 @@
+import expect from 'expect';
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import TestUtils from 'react-addons-test-utils';
+import CourseForm from './CourseForm';
+
+
+let setup = function(saving){
+    const props = {
+        course: {},
+        saving: saving,
+        errors: {},
+        onSave: () => {},
+        onChange: () => {}
+    };
+
+    return shallow(<CourseForm {...props} />);
+};
+
+
+describe('CourseForm via Enzyme', ()=>{
+    it('renders form and h1', () => {
+        const wrapper = setup(false);
+        expect(wrapper.find('input').props().value).toBe('Save');
+    });
+
+    it('save button is labeled "Saving..." when saving', ()=>{
+        const wrapper = setup(true);
+        expect(wrapper.find('input').props().value).toBe('Saving...');
+    });
+});
